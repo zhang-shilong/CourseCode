264fbd7f2157af59a9f1129cb5c4fa22e14e2f5d
zhang-shilong <zhang_shilong@outlook.com>
2022-06-27 10:02:19 +0800

    add WebDevelopment

--- WebDevelopment/sql/web_class_login.sql
+++
@@ -1,9 +0,0 @@
-create table login
-(
-    id       int unsigned auto_increment
-        primary key,
-    username text null,
-    password text null
-);
-
-INSERT INTO web_class.login (id, username, password) VALUES (1, 'zsl', 'zslzsl');
